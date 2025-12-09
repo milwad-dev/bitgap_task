589dae7bbdb5cfbd6a1ddf10b5eb31a1d506d06b
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-12-09 14:05:55 +0330

    Create SignUpTest.php

---
+++ tests/Feature/Auth/SignUpTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\User;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\postJson;
+
+test('guest user can sign up', function () {
+    $response = postJson(route('auth.sign-up'), [
+        'name' => 'Milwad Khosravi',
+        'email' => 'milwad@gmail.com',
+        'password' => 'Milwad123!',
+    ]);
+    $response->assertOk();
+    $response->assertJsonStructure([
+        'status',
+        'data' => [
+            'token',
+            'user' => [
+                'id',
+                'name',
+                'email',
+            ],
+        ],
+    ]);
+
+    // DB Assertions
+    assertDatabaseCount('users', 1);
+});
