0ff0cb0250365edfa0538b893b89cc79885bb4a1
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-12-09 16:45:52 +0330

    Create TaskResource.php

---
+++ app/Http/Resources/Task/TaskResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Task;
+
+use App\Http\Resources\User\UserResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'status' => $this->status,
+            'description' => $this->description,
+            'due_date' => $this->due_date,
+            'assigned' => new UserResource($this->whenLoaded('assigned')),
+        ];
+    }
+}
