70ad1bdedfc0021e3b98d844466b647aee74ebdf
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-12-09 16:15:42 +0330

    Create RolePermissionStoreTest.php

---
+++ tests/Feature/RolePermission/RolePermissionStoreTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Permission;
+use App\Models\User;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\postJson;
+
+test('login user can store new role', function () {
+    $user = User::factory()->create();
+    Permission::query()->create(['name' => Permission::PERMISSION_TASK_VIEW]);
+    Permission::query()->create(['name' => Permission::PERMISSION_TASK_CREATE]);
+
+    $response = actingAs($user)->postJson(route('role-permissions.store', [
+        'name' => 'NEW ROLE',
+        'permissions' => [1, 2]
+    ]));
+    $response->assertCreated();
+
+    // DB Assertions
+    assertDatabaseCount('roles', 1);
+});
+
+test('guest user can not store new role', function () {
+    $response = postJson(route('role-permissions.store', [
+        'name' => 'NEW ROLE',
+        'permissions' => [1, 2]
+    ]));
+    $response->assertUnauthorized();
+
+    // DB Assertions
+    assertDatabaseCount('roles', 0);
+});
