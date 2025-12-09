208c3caf8c397af4800eae0d2b5269f2d0584c5a
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-12-09 16:37:08 +0330

    Create Task.php

---
+++ app/Models/Task.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /** @use HasFactory<\Database\Factories\TaskFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'status',
+        'description',
+        'due_date',
+        'assigned_id',
+    ];
+
+    // Relations
+
+    /**
+     * Relation one-to-many, User model.
+     */
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * Relation one-to-many, User model.
+     */
+    public function assigned(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'assigned_id');
+    }
+}
