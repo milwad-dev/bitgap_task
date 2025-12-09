7582e64bc5a4e4b421a5561056c652961f25bcbd
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-12-09 13:50:46 +0330

    Create SignInController.php

---
+++ app/Http/Controllers/Auth/SignInController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\SignInRequest;
+use App\Http\Resources\User\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class SignInController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(SignInRequest $request)
+    {
+        $user = User::query()->where('email', $request->email)->firstOrFail();
+
+        // Check passwords are equal
+        if (! Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        return response()->json([
+            'status' => 'success',
+            'data' => [
+                'token' => $token,
+                'user' => new UserResource($user),
+            ],
+        ]);
+    }
+}
