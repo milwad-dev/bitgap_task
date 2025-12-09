238722690bd4f8365b23b015a65edaad38ee9db2
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-12-09 17:32:16 +0330

    add `guest user can not delete task` test

--- tests/Feature/Task/TaskDeleteTest.php
+++ tests/Feature/Task/TaskDeleteTest.php
@@ -4,14 +4,25 @@ use App\Models\Task;
 use App\Models\User;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\deleteJson;
 
 test('login user can delete task', function () {
     $user = User::factory()->create();
     $task = Task::factory()->create();
 
-    $response = actingAs($user)->delete(route('tasks.destroy', $task->getKey()));
+    $response = actingAs($user)->deleteJson(route('tasks.destroy', $task->getKey()));
     $response->assertNoContent();
 
     // DB Assertions
     assertDatabaseCount('tasks', 0);
+});
+
+test('guest user can not delete task', function () {
+    $task = Task::factory()->create();
+
+    $response = deleteJson(route('tasks.destroy', $task->getKey()));
+    $response->assertUnauthorized();
+
+    // DB Assertions
+    assertDatabaseCount('tasks', 1);
 });
