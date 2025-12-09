8bde4cf1db9f1ac122d22138bde06a59535b6794
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-12-09 22:29:14 +0330

    Create AuditLogActionEnum.php

---
+++ app/Enums/AuditLog/AuditLogActionEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums\AuditLog;
+
+enum AuditLogActionEnum: string
+{
+    case ACTION_READ = 'read';
+    case ACTION_CREATE = 'create';
+    case ACTION_UPDATE = 'update';
+    case ACTION_DELETE = 'delete';
+}
