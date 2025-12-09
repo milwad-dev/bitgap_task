f9773e4a7254b489f8af1667c83d95229999c47b
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-12-09 16:10:35 +0330

    add `guest user can not see roles index` test

--- tests/Feature/RolePermission/RolePermissionIndexTest.php
+++ tests/Feature/RolePermission/RolePermissionIndexTest.php
@@ -2,10 +2,16 @@
 
 use App\Models\User;
 use function Pest\Laravel\actingAs;
+use function Pest\Laravel\getJson;
 
 test('login user can see roles index', function () {
     $user = User::factory()->create();
 
     $response = actingAs($user)->getJson(route('role-permissions.index'));
     $response->assertOk();
+});
+
+test('guest user can not see roles index', function () {
+    $response = getJson(route('role-permissions.index'));
+    $response->assertUnauthorized();
 });
