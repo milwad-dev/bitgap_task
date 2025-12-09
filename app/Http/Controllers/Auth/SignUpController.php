e57bca0d77d405b56ad7c838210c4d375b1ea03d
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-12-09 23:06:40 +0330

    Update SignUpController.php

--- app/Http/Controllers/Auth/SignUpController.php
+++ app/Http/Controllers/Auth/SignUpController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Enums\AuditLog\AuditLogActionEnum;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\SignUpRequest;
 use App\Http\Resources\User\UserResource;
 use App\Models\User;
+use App\Services\AuditLog\AuditLogService;
 use Illuminate\Support\Facades\Hash;
 
 class SignUpController extends Controller
@@ -15,13 +17,18 @@ class SignUpController extends Controller
      */
     public function __invoke(SignUpRequest $request)
     {
+        // Create user
         $user = User::query()->create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
+        // Create user token
         $token = $user->createToken('token')->plainTextToken;
+
+        // Create log
+        resolve(AuditLogService::class)->store(AuditLogActionEnum::ACTION_REGISTER->value);
 
         return response()->json([
             'status' => 'success',
