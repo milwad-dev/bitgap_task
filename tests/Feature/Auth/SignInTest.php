38caeed6f9bc0b0a72d90b5d83adc00fcc14854e
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-12-09 13:58:10 +0330

    add `login user can not sign in` test

--- tests/Feature/Auth/SignInTest.php
+++ tests/Feature/Auth/SignInTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\User;
+use function Pest\Laravel\actingAs;
 use function Pest\Laravel\postJson;
 
 test('guest user can sign in', function () {
@@ -22,4 +23,14 @@ test('guest user can sign in', function () {
             ],
         ],
     ]);
+});
+
+test('login user can not sign in', function () {
+    $user = User::factory()->create(['password' => Hash::make('password')]);
+
+    $response = actingAs($user, 'sanctum')->postJson(route('auth.sign-in'), [
+        'email' => $user->email,
+        'password' => 'password',
+    ]);
+    $response->assertFound();
 });
