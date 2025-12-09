03de83666fcd100ad7e459af6e50441032ae55e5
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-12-09 13:43:34 +0330

    add sign-up route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\SignUpController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// SignUp
+Route::post('sign-up', SignUpController::class)->middleware('guest:sanctum');
