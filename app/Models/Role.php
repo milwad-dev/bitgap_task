cf37fb96b4917cf0b40eea36b9cb784674d35ffa
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-12-09 15:41:31 +0330

    Update Role.php

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Role extends \Spatie\Permission\Models\Role
 {
     use HasFactory;
+
+    /**
+     * The relations to eager load on every query.
+     *
+     * @var string[]
+     */
+    protected $with = ['permissions'];
 }
